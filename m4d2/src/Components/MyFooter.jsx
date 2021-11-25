f661c0958ad33690482ab192ae818cb960235b8b
Bilal Ahmad <bilal.ahmad0796@gmail.com>
2021-11-25 15:22:34 +0100

    App created

---
+++ m4d2/src/Components/MyFooter.jsx
@@ -0,0 +1,7 @@
+//import {footer} from 'react-bootstrap';
+const MyFooter = () => (
+     <footer className="d-flex bg-dark">
+        <span className="text-white m-auto"><strong>Strive School</strong> - Copyright 2021</span>
+    </footer>
+)
+export default MyFooter
